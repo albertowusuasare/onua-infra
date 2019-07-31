146ac1bbea750054c49929fbfca5087f88bce5fb
albertowusuasa <owusuasa@grinnell.edu>
2019-07-31 14:01:21 +0100

    fix init path

--- customer-app/cloud-func/deploy.sh
+++ customer-app/cloud-func/deploy.sh
@@ -9,7 +9,7 @@ cd $DEPLOY_PATH
 git clone https://github.com/albertowusuasare/customer-app.git
 cd ./customer-app/cmd/customer-svc-func/google
 git checkout $SOURCE_VERSION
-go mod init
+go mod init github.com/albertowusuasare/customer-app/cmd/customer-svc-func/google
 go mod tidy
 go mod vendor
 gcloud functions deploy google --entry-point Handle --runtime go111 --trigger-http --set-env-vars PROJECT_ID=onua-246719
