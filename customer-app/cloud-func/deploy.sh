7e6ffa567fb0b7e96de67e8fce274598c964e415
albertowusuasa <owusuasa@grinnell.edu>
2019-08-08 15:23:11 +0100

    add module pkg name

--- customer-app/cloud-func/deploy.sh
+++ customer-app/cloud-func/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export GO111MODULE=on
-SOURCE_VERSION=v0.0.9
+export MOD_PKG_NAME=functions
+SOURCE_VERSION=v0.1.2
 DEPLOY_PATH=customer-app-deploy
 HOME=$(pwd)
 FUNCTION_PATH=customer-app/cmd/customer-svc-func-gcp
@@ -9,8 +9,7 @@ cd $DEPLOY_PATH
 git clone https://github.com/albertowusuasare/customer-app.git
 cd ./$FUNCTION_PATH
 git checkout $SOURCE_VERSION
-go mod tidy
-go mod vendor
+$HOME/$DEPLOY_PATH/customer-app/script/gomod.sh 
 gcloud functions deploy customer-svc-func-gcp --entry-point Handle --runtime go111 --trigger-http --set-env-vars PROJECT_ID=onua-246719
 cd $HOME
 rm -rf $DEPLOY_PATH
