3340efd9f55ecbb07858cd95ac52016b415b7632
Albert Owusu-Asare <albertowusu-asare@alberts-air.mynet>
2019-08-11 13:54:51 +0100

    Source version update to v0.18.0

--- customer-app/cloud-func/deploy.sh
+++ customer-app/cloud-func/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 MOD_PKG_NAME=functions
 PROJECT_ID=onua-246719
-SOURCE_VERSION=/Users/albertowusu-asare/go/src/github.com/albertowusuasare/customer-app/script
+SOURCE_VERSION=v0.18.0
 DEPLOY_PATH=customer-app-deploy
 HOME=$(pwd)
 FUNCTION_PATH=customer-app/functions
