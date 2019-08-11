e02d113e2fc51f84bf0ae79eb60d974bf3c4cf63
Albert Owusu-Asare <albertowusu-asare@alberts-air.mynet>
2019-08-11 09:39:06 +0100

    Source version update

--- customer-app/cloud-func/deploy.sh
+++ customer-app/cloud-func/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export MOD_PKG_NAME=functions
 PROJECT_ID=onua-246719
-SOURCE_VERSION=v0.2.0
+SOURCE_VERSION=v0.4.0
 DEPLOY_PATH=customer-app-deploy
 HOME=$(pwd)
 FUNCTION_PATH=customer-app/cmd/customer-svc-func-gcp
@@ -9,7 +9,7 @@ mkdir $DEPLOY_PATH
 cd $DEPLOY_PATH
 git clone https://github.com/albertowusuasare/customer-app.git
 cd ./$FUNCTION_PATH
-git checkout $SOURCE_VERSION
+SOURCE_VERSION=v0.4.0
 $HOME/$DEPLOY_PATH/customer-app/script/gomod.sh 
 gcloud functions deploy customer-svc-func-gcp --entry-point Handle --runtime go111 --trigger-http --set-env-vars PROJECT_ID=$PROJECT_ID
 cd $HOME
