e9aefcd02acc2e4c9b4990a304d3441fe3c4a379
albertowusuasa <owusuasa@grinnell.edu>
2019-08-11 10:10:20 +0100

    add function pkg name as a parameter to module script

--- customer-app/cloud-func/deploy.sh
+++ customer-app/cloud-func/deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-export MOD_PKG_NAME=functions
+MOD_PKG_NAME=functions
 PROJECT_ID=onua-246719
 SOURCE_VERSION=v0.7.0
 DEPLOY_PATH=customer-app-deploy
 HOME=$(pwd)
-FUNCTION_PATH=customer-app/cmd/customer-svc-func-gcp
+FUNCTION_PATH=customer-app/functions
 mkdir $DEPLOY_PATH
 cd $DEPLOY_PATH
 git clone https://github.com/albertowusuasare/customer-app.git
 cd ./$FUNCTION_PATH
 git checkout $SOURCE_VERSION
-$HOME/$DEPLOY_PATH/customer-app/script/gomod.sh 
+$HOME/$DEPLOY_PATH/customer-app/script/gomod.sh $MOD_PKG_NAME
 gcloud functions deploy customer-svc-func-gcp --entry-point Handle --runtime go111 --trigger-http --set-env-vars PROJECT_ID=$PROJECT_ID
 cd $HOME
 rm -rf $DEPLOY_PATH
